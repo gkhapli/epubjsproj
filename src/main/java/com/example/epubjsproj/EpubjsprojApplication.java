7433607cbef060f5590af4d0295a77ba1d442119
gkhapli <gkhapli@gmail.com>
2021-04-02 07:38:55 +0200

    Initial commit

---
+++ src/main/java/com/example/epubjsproj/EpubjsprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.epubjsproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpubjsprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpubjsprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/epubjsproj/EpubjsprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epubjsproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpubjsprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
